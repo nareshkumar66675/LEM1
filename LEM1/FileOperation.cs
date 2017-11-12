2f12de07bf6e9d4745d051e9cb2a5616b35d8dde
Naresh Kumar <nareshkumar66675@gmail.com>
2017-11-11 21:15:44 -0600

    Added File Operation to perform File operations and parsing

---
+++ LEM1/FileOperation.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LEM1
+{
+    public static class FileOperation
+    {
+        public static DataTable ReadDataFile(string path)
+        {
+            DataTable data = new DataTable();
+            if (File.Exists(path))
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    string line = string.Empty;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        switch (line[0])
+                        {
+                            case '<':
+                             break;
+                            case '!':
+                                break;
+                            case '[':
+                                ParseHeaders(line, data);
+                                break;
+                            default:
+                                ParseLine(line,data);
+                                break;
+                        }
+                    }
+                }
+
+            }
+            else
+            {
+                throw new Exception("Data File Missing - "+ Path.GetFileName(path));
+            }
+            return data;
+        }
+        private static void ParseHeaders(string line,DataTable data)
+        {
+            var colHeaders = line.Trim().Replace('<', ' ').Replace('>', ' ').Split(new char[0], StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            colHeaders.ForEach(t => data.Columns.Add(new DataColumn(t, typeof(string))));
+        }
+        private static void ParseLine(string line, DataTable data)
+        {
+            var values = line.Trim().Split(new char[0]);
+            if (values.Length == data.Columns.Count)
+            {
+                data.Rows.Add(values);
+            }
+            else
+                throw new Exception("Column and Data count Mismatch");
+
+        }
+    }
+}
